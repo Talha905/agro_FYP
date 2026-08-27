a03392ee805d8971fcd00c55e19a21436f961f8e
talha <t2240794@gmail.com>
2026-08-27 20:28:43 +0530

    fix(main.dart): import package:flutter/cupertino.dart for CupertinoPageTransitionsBuilder

--- agrosaathi/lib/main.dart
+++ agrosaathi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
